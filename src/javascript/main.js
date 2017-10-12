afb8507702b2dfaa4e4c46df28fb07b9fe541089
dh-dev-prog <Damien@MacBook-Pro-de-Damien.local>
2017-10-13 07:13:27 +0900

    Fix responsive images

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,40 +1,5 @@
 'use strict';
 
-/*var group = document.querySelector('.banner-carousel')
-var items = document.querySelectorAll('.banner-carousel-item');
-var currentIndex = 0;
-var timer;
-
-advance()
-
-function advance() {
-  clearTimeout(timer)
-  var timer = setTimeout(function () {
-    if(currentIndex < items.length - 1) {
-      move(currentIndex + 1)
-    } else {
-      move(0)
-    }
-  }, 1000);
-}
-
-
-function move(newIndex){
-  advance()
-  if (newIndex > currentIndex) {
-    items[currentIndex].style.transform = 'translate(-100%)';
-    items[currentIndex].style.display = 'none';
-    items[newIndex].style.display = 'block';
-  } else {
-    items[currentIndex].style.transform = 'translate(-100%)';
-    items[currentIndex].style.display = 'none';
-    items[newIndex].style.display = 'block';
-    items[newIndex].style.transform = 'translate(0)';
-  }
-
-  currentIndex = newIndex;
-}*/
-
 var slideIndex = 0;
 showSlides();
 
@@ -50,3 +15,16 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+function navLink(){
+  var links = document.querySelectorAll('.main-nav-link');
+  links.forEach(function(link){
+    link.addEventListener('click', function(){
+      links.forEach(function(link){
+        link.classList.remove('is-active')
+      })
+      link.classList.add('is-active')
+    })
+  })
+}
+navLink();
